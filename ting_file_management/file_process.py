4163e191d0a5ac35e973b94d999b9b0e381b37a9
Brayan Santos <brayan.santosrosa@gmail.com>
2023-04-04 17:05:58 -0300

    Requisito 03 concluido

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,25 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    linhas = txt_importer(path_file)
+    obj = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(linhas),
+        "linhas_do_arquivo": linhas
+    }
+    if len(instance) == 0:
+        instance.enqueue(obj)
+        print(obj, file=sys.stdout)
+    if len(instance) != 0:   
+        for i in range(len(instance)-1):
+            result = instance.search[i]
+            if result['nome_do_arquivo'] == path_file:
+                return None
+    else:
+        instance.enqueue(obj)
+        print(obj, file=sys.stdout)
 
 
 def remove(instance):
@@ -8,3 +28,6 @@ def remove(instance):
 
 def file_metadata(instance, position):
     """Aqui irá sua implementação"""
+
+
+#
