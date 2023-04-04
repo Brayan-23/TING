9aaad8f2389662a68a7461a6b76fda1ec0d1782a
Brayan Santos <brayan.santosrosa@gmail.com>
2023-04-04 15:55:42 -0300

    Requisito 02 concluido

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,12 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith('.txt'):
+        print('Formato inválido', file=sys.stderr)
+    try:
+        with open(path_file, encoding='utf-8', mode='r') as file:
+            leitura = file.read().split('\n')
+            return leitura
+    except FileNotFoundError:
+        print(f'Arquivo {path_file} não encontrado', file=sys.stderr)
