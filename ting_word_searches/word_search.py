21df62181d91d3f6be311d895156231a11c26c9a
Brayan Santos <brayan.santosrosa@gmail.com>
2023-04-05 11:36:46 -0300

    Requsito 07 concluido

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -17,4 +17,18 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    lista_de_objetos = []
+    lista_de_linhas = []
+    for i in instance.data:
+        for index, row in enumerate(i['linhas_do_arquivo']):
+            if word.lower() in row.lower():
+                lista_de_linhas.append({"linha": index+1, "conteudo": row})
+                continue
+        if len(lista_de_linhas) != 0:
+            lista_de_objetos.append({
+                "palavra": word,
+                "arquivo": i['nome_do_arquivo'],
+                "ocorrencias": lista_de_linhas
+            })
+        print(lista_de_objetos)
+    return lista_de_objetos
